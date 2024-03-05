8057a4f844d46dfa892c1713444e0565660a0444
Chris <christopherndugo@gmail.com>
2024-03-06 00:11:04 +0300

    set up auth0 template

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,42 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 'use client'
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import Link from "next/link";
 
-function index() {
+function Index() {
   const { user, error, isLoading } = useUser();
+  const [accessToken, setAccessToken] = useState(null);
+
+  useEffect(() => {
+    const fetchAccessToken = async () => {
+      const response = await fetch('/api/auth/token');
+      const { accessToken } = await response.json();
+      setAccessToken(accessToken);
+    };
+    if (user) {
+      fetchAccessToken();
+    }
+
+  }, [user]);
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
 
   if (user) {
     console.log(user);
+    console.log(accessToken);
+
     return (
       <div>
-    <img src="https://lh3.googleusercontent.com/d/1U8PvAzjhZ9jDUPvtHk9BYTJ0qvVz0xF3" alt="Description of the image"/>
-
-        Welcome {user.name}! <Link  href="/api/auth/logout">Logout</Link>
-        <br></br>
+        Welcome {user.name}! <Link href="/api/auth/logout">Logout</Link>
+        <br />
         Your nickname is {user.nickname}.
       </div>
     );
   }
+
   return <Link href="/api/auth/login">Login</Link>;
 }
 
-export default index;
+export default Index;
